0f01da51c8ce3d0a840f7c4521a40b4fabaa9a6c
Ian Shearer <iangcshearer@gmail.com>
2022-04-28 08:39:41 -0400

    lambda-rfc3966 returns a json quoted phone number

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/StirlingMarketingGroup/lambda-rfc3966
+
+go 1.18
+
+require (
+	github.com/aws/aws-lambda-go v1.31.1
+	github.com/nyaruka/phonenumbers v1.0.74
+)
+
+require (
+	github.com/golang/protobuf v1.3.2 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/nyaruka/phonenumbers"
+)
+
+func handler(phoneNumber string) (string, error) {
+	p, err := phonenumbers.Parse(phoneNumber, "US")
+	if err != nil {
+		return "", err
+	}
+
+	if !phonenumbers.IsValidNumber(p) {
+		return "", err
+	}
+
+	parsedPhone := strings.TrimPrefix(phonenumbers.Format(p, phonenumbers.RFC3966), "tel:")
+
+	jsonPhone, err := json.Marshal(parsedPhone)
+	if err != nil {
+		return "", err
+	}
+
+	return string(jsonPhone), nil
+}
+
+func main() {
+	if len(os.Getenv("AWS_EXECUTION_ENV")) == 0 {
+		phoneNumber := os.Args[1]
+		p, err := handler(phoneNumber)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(p)
+		}
+		return
+	}
+
+	lambda.Start(handler)
+}
